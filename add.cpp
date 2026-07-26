f11ab59980754f135429e0e80d0ccf637ec5d7fd
siddhisardesai561-cell <siddhisardesai561@gmail.com>
2026-07-26 18:13:59 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-int a, b;
-   cout << " a+b";
-}  
+int main() {
+    int a, b, sum;
+
+    cout<<"Enter a : ";
+    cin>>a;
+
+    cout<<"Enter b : ";
+    cin>>b;
+
+    sum = a + b;
+    cout<<"Sum : "<<sum;
+    return 0;
+} 
 
