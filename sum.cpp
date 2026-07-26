061f2d2892a2bda68333467e4647a839174fc9bd
siddhisardesai561-cell <siddhisardesai561@gmail.com>
2026-07-26 17:58:22 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -5,10 +5,10 @@ int main()
 int a, b;
    cout << "Enter two numbers:";
    cin>>a>>b;
-   cout<<"Addition"<<a+b<<endl;
-   cout<<"Subtraction"<<a-b<<endl;
-   cout<<"Multiplication"<<a*b<<endl;
-   cout<<"Division"<<a/b<<endl;
-   cout<<"Modulo"<<a%b<<endl; 
+   cout<<"Addition : "<<a+b<<endl;
+   cout<<"Subtraction : "<<a-b<<endl;
+   cout<<"Multiplication : "<<a*b<<endl;
+   cout<<"Division : "<<a/b<<endl;
+   cout<<"Modulo : "<<a%b<<endl; 
 }  
 
